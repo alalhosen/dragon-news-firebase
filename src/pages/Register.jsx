f976b340029cf03c63c34209f15494ddabdeccf7
Alal Hosen <alalhosen1992620120@gmail.com>
2026-08-13 22:22:01 +0600

    create register component & function setup

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router";
 
 const Register = () => {
-    return (
-        <div>
-            <h2>Register</h2>
+  return (
+    <div className="flex justify-center min-h-screen items-center">
+      <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl py-5">
+        <h2 className="font-semibold text-2xl text-center">
+          Register your account
+        </h2>
+        <div className="card-body">
+          <fieldset className="fieldset">
+            <label className="label">Email</label>
+            <input type="email" className="input" placeholder="Email" />
+            <label className="label">Password</label>
+            <input type="password" className="input" placeholder="Password" />
+
+            <button className="btn btn-neutral mt-4">Register</button>
+            <p className="font-semibold text-center pt-5">
+              Dont't Have An Account ?{" "}
+              <Link className="text-secondary" to="/auth/login">
+                Login
+              </Link>
+            </p>
+          </fieldset>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Register;
