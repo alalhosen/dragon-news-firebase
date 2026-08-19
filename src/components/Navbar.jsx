be0b31fd88a68acb4a36905ec887e493bf0a0699
Alal Hosen <alalhosen1992620120@gmail.com>
2026-08-19 23:54:53 +0600

    use loop function

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,10 +3,10 @@ import { Link, NavLink } from "react-router";
 import user from "../assets/user.png";
 import { AuthContext } from "../provider/AuthProvider";
 const Navbar = () => {
-  const {user}= use(AuthContext )
+  const { user } = use(AuthContext);
   return (
     <div className="flex justify-between items-center">
-      <div className=""></div>
+      <div className="">{user && user.email}</div>
       <div className="nav flex gap-5 text-accent">
         <NavLink to="/">Home</NavLink>
         <NavLink to="/about">About</NavLink>
@@ -14,7 +14,9 @@ const Navbar = () => {
       </div>
       <div className="login-btn flex gap-5">
         <img src={user} alt="" />
-        <Link to="/auth/login" className="btn btn-primary px-10 ">Login</Link>
+        <Link to="/auth/login" className="btn btn-primary px-10 ">
+          Login
+        </Link>
       </div>
     </div>
   );
