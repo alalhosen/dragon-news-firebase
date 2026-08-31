2b1c358e9e49e8b395fd1bef926b7e2126896551
Alal Hosen <alalhosen1992620120@gmail.com>
2026-08-31 23:30:42 +0600

    create PrivateRoute component & set newsDetails component in the PrivateRoute component

---
+++ src/provider/PrivateRoute.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { AuthContext } from "./AuthProvider";
+
+const PrivateRoute = ({ children }) => {
+    const {user}= use (AuthContext)
+    console.log(user)
+    // if -> amader user thake tahole return korbo children ke
+  return children;
+//   if amader user na thake, tahole navigate kore dibo login e 
+};
+
+export default PrivateRoute;
