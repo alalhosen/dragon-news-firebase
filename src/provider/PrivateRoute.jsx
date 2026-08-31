0ea5466866eb19ccaeec0e47221657704480a524
Alal Hosen <alalhosen1992620120@gmail.com>
2026-08-31 23:35:39 +0600

    use problem slove

--- src/provider/PrivateRoute.jsx
+++ src/provider/PrivateRoute.jsx
@@ -1,12 +1,12 @@
-import React from "react";
+import React, { use } from "react";
 import { AuthContext } from "./AuthProvider";
 
 const PrivateRoute = ({ children }) => {
-    const {user}= use (AuthContext)
-    console.log(user)
-    // if -> amader user thake tahole return korbo children ke
+  const { user } = use(AuthContext);
+  console.log(user);
+  // if -> amader user thake tahole return korbo children ke
   return children;
-//   if amader user na thake, tahole navigate kore dibo login e 
+  //   if amader user na thake, tahole navigate kore dibo login e
 };
 
 export default PrivateRoute;
