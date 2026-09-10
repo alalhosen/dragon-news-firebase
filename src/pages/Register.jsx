f2c92696a2d29eaaba77768a21c3a2d26fb1089c
Alal Hosen <alalhosen1992620120@gmail.com>
2026-09-10 21:56:06 +0600

    firebase deploy

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -10,7 +10,7 @@ const Register = () => {
 
   const handleRegister = (e) => {
     e.preventDefault();
-    console.log(e.target);
+    // console.log(e.target);
     const form = e.target;
     const name = form.name.value;
     if (name.length < 5) {
