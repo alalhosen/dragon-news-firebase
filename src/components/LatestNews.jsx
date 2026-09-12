c620b1207b5c5ccfd05cd728e21d5a8d45b7c9a8
Alal Hosen <alalhosen1992620120@gmail.com>
2026-09-12 23:24:03 +0600

    change latest news text

--- src/components/LatestNews.jsx
+++ src/components/LatestNews.jsx
@@ -6,19 +6,21 @@ const LatestNews = () => {
     <div className="flex items-center gap-5 bg-base-200 p-3">
       <p className="text-base-100 bg-secondary px-3 py-2">Latest</p>
 
-      <Marquee className="flex gap-5" pauseOnHover={true} speed={60}>
+      <Marquee className="flex gap-15" pauseOnHover={true} speed={60}>
+
         <p className="font-bold">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam,
+          Lorem ipsum dolor t. Aliquam,
           accusantium?
         </p>
         <p className="font-bold">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam,
+          Lorem ipsum Aliquam,
           accusantium?
         </p>
         <p className="font-bold">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam,
+          Lorem ipsum dolor quam,
           accusantium?
         </p>
+        
       </Marquee>
     </div>
   );
