14193116fd7a7cb461131591b02397fdc6c220b3
Alal Hosen <alalhosen1992620120@gmail.com>
2026-09-14 21:25:43 +0600

    some change for latest news

--- src/components/LatestNews.jsx
+++ src/components/LatestNews.jsx
@@ -2,15 +2,14 @@ import React from "react";
 import Marquee from "react-fast-marquee";
 
 const LatestNews = ({ latestNews }) => {
-  const { title } = latestNews;
+  // const { title } = latestNews;
+  const title = latestNews?.title || "Loading news...";
   return (
     <div className="flex items-center gap-5 bg-base-200 p-3">
       <p className="text-base-100 bg-secondary px-3 py-2">Latest</p>
 
       <Marquee className="flex gap-15" pauseOnHover={true} speed={60}>
-        <p className="font-bold">Lorem ipsum dolor t. Aliquam, accusantium?</p>
-        <p className="font-bold">Lorem ipsum Aliquam, accusantium?</p>
-        <p className="font-bold">Lorem ipsum dolor quam, accusantium?</p>
+        <p className="font-bold">{title}</p>
       </Marquee>
     </div>
   );
--- src/components/NewsCard.jsx
+++ src/components/NewsCard.jsx
@@ -10,6 +10,8 @@ const NewsCard = ({ news }) => {
 
   return (
     <div className="card bg-base-100 shadow-md mb-6">
+
+      
       {/* Author + Share */}
       <div className="flex bg-base-200 justify-between items-center p-4">
         <div className="flex items-center gap-3">
