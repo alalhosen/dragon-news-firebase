f9d536e1125cda2ed7e7bb2a7e44202d1bf35f93
Alal Hosen <alalhosen1992620120@gmail.com>
2026-09-15 23:25:36 +0600

    latest breaking news title set

--- src/components/LatestNews.jsx
+++ src/components/LatestNews.jsx
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Marquee from "react-fast-marquee";
 
-const LatestNews = ({ latestNews }) => {
-  // const { title } = latestNews;
-  const title = latestNews?.title || "Loading news...";
+const LatestNews = () => {
+  const [news, setNews] = useState([]);
+
+  useEffect(() => {
+    fetch("/public/news.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setNews(data);
+      });
+  }, []);
   return (
     <div className="flex items-center gap-5 bg-base-200 p-3">
       <p className="text-base-100 bg-secondary px-3 py-2">Latest</p>
 
       <Marquee className="flex gap-15" pauseOnHover={true} speed={60}>
-        <p className="font-bold">{title}</p>
+        {news.map((item) => (
+          <p key={item._id}>{item.title}</p>
+        ))}
       </Marquee>
     </div>
   );
