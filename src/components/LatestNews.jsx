30e2a40eca958f886cfd079457aac9257ba9b69a
Alal Hosen <alalhosen1992620120@gmail.com>
2026-09-16 23:53:58 +0600

    latest news update

--- src/components/LatestNews.jsx
+++ src/components/LatestNews.jsx
@@ -5,7 +5,7 @@ const LatestNews = () => {
   const [news, setNews] = useState([]);
 
   useEffect(() => {
-    fetch("/public/news.json")
+    fetch("/news.json")
       .then((res) => res.json())
       .then((data) => {
         setNews(data);
